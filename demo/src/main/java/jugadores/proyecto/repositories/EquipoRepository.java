82e41d416f2481a034a7a3ca1ee8274dd575014a
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2024-11-27 17:37:54 -0500

    Delete demo directory

--- demo/src/main/java/jugadores/proyecto/repositories/EquipoRepository.java
+++
@@ -1,7 +0,0 @@
-package jugadores.proyecto.repositories;
-
-import jugadores.proyecto.models.Equipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipoRepository extends JpaRepository<Equipo,Integer> {
-}
